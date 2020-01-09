0733d0342b8dca3a6e95bae4c41c267fe5441047
b0kch01 <nath49ers@gmail.com>
2020-01-09 10:55:43 -0800

    Creation

---
+++ FracCalc/frac_calc.py
@@ -0,0 +1,17 @@
+def produce_answer(input):
+    for charachter in input.split(""):
+        if charchter not in "+-*123456789_/ ":
+            return "Error: I found an invalid symbol"
+    if input == "" or "  " in input or input[0] == " ":
+        return "Error: Spaces = Nothing = Not a correct expression."
+
+    inputs = input.split(" ")
+
+    if len(inputs) <= 2:
+        return "Error: Not an expression or you forgot to add a space."
+    else:
+        out = []
+        group = 0
+        while group < len(inputs / 2):
+            group++
+            toMixed(reduceFraction(out))
